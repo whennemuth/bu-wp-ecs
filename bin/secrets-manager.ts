2e2103318fceb42ec90c07d29811879f5125d769
whennemuth <wrh@bu.edu>
2025-12-29 15:01:28 -0500

    Adding parameters for createOrUpdateSecrets to override values derived from context.json

--- bin/secrets-manager.ts
+++ bin/secrets-manager.ts
@@ -13,26 +13,46 @@ export const createOrUpdateSecrets = async () => {
   const context = contextJSON as IContext;
 
   const { 
-    STACK_ID, REGION, TAGS: { Landscape }, WORDPRESS: { 
+    STACK_ID, REGION, WORDPRESS: { 
       secret: { fields: { configExtra, dbPassword } }, env: { dbUser } 
     } } = context;
 
-  const secretName = `${STACK_ID}/${Landscape}`;
-  const fldNames = { configExtra, dbPassword, spCert: 'N/A', spKey: 'N/A' } satisfies SecretFieldNames;
-  const description = `Stores wordpress database username and password, and wp-config-extra, which contains sensitive values`;
+  // Landscape can be overridden by env variable LANDSCAPE.
+  let { TAGS: { Landscape } } = context;
 
-  const { CONFIG_EXTRA, DB_PASSWORD } = process.env;
 
-  if( ! CONFIG_EXTRA ) {
-    throw new Error('CONFIG_EXTRA environment variable must be set');
+  const { WORDPRESS_CONFIG_EXTRA, DB_PASSWORD } = process.env;
+
+  if( ! WORDPRESS_CONFIG_EXTRA ) {
+    throw new Error('WORDPRESS_CONFIG_EXTRA environment variable must be set');
   }
 
   if( ! DB_PASSWORD ) {
     throw new Error('DB_PASSWORD environment variable must be set');
   }
 
-  const smSecret = new SecretsManagerSecret({ secretName, description, fldNames, region: REGION })
-    .setValue(configExtra, CONFIG_EXTRA)
+  const args = process.argv.slice(1);
+  let secretNameOverride = '';
+  args.forEach((arg) => {
+    const [key, value] = arg.split('=');
+    if( key.trim().toUpperCase() === 'LANDSCAPE' ) {
+      console.log(`Overriding Landscape tag from ${Landscape} from context to ${value} from argument`);
+      Landscape = value;
+    }
+    if( key.trim().toUpperCase() === 'SECRET_NAME' ) {
+      console.log(`Overriding entire secret name to ${value} from argument`);
+      secretNameOverride = value;
+    }
+  });
+
+  const secretName = secretNameOverride || `${STACK_ID}/${Landscape}`;
+  const fldNames = { configExtra, dbPassword, spCert: 'N/A', spKey: 'N/A' } satisfies SecretFieldNames;
+  const description = `Stores wordpress database username and password, and wp-config-extra, which contains sensitive values`;
+
+  const smSecret = new SecretsManagerSecret({ secretName, description, fldNames, region: REGION });
+
+  smSecret
+    .setValue(configExtra, WORDPRESS_CONFIG_EXTRA)
     .setValue(dbPassword, DB_PASSWORD)
     .setValue('username', dbUser || 'root');
 
